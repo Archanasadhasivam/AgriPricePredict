69f3ac393b1a0f8c5be66fd4f1582e14017a889d
Harsithask <148435108+Harsithask@users.noreply.github.com>
2025-05-25 23:39:16 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,10 +1,11 @@
 <?php
-$servername = "localhost";
-$username = "root"; // Default for XAMPP
-$password = ""; // Default is empty for XAMPP
-$database = "price_prediction";
+$host = "mysql-fc0e3b0-sadhasivamkanaga15-f154.l.aivencloud.com";
+$port = 21436;
+$user = "avnadmin";
+$password = "AVNS_P2X1P7jH__WuLtv9YSs"; // IMPORTANT: Replace with your actual Aiven password!
+$dbname = "defaultdb";
 
-$conn = new mysqli($servername, $username, $password, $database);
+$conn = new mysqli($host, $user, $password, $dbname, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
